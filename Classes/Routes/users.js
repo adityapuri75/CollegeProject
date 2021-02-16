937cbf562c996d90ec60f688a8412f23f637c865
adityapuri75 <adityapuri75@gmail.com>
2021-02-16 22:32:36 +0530

    User Database Done

--- Classes/Routes/users.js
+++ Classes/Routes/users.js
@@ -4,6 +4,25 @@ const router = express.Router();
 const User = require("../Model/user");
 
 
+router.post("/", async function (req, res) {
+  const newUser = {
+    name: req.body.displayName,
+    image: req.body.image,
+    googleId: req.body.email,
+    group: req.body.group,
+    section: req.body.section,
+    university: req.body.university,
+  };
+
+  let user = await User.findOne({ googleId: req.body.email });
+
+  if (user) {
+    res.send({ message: "User Already In Database" });
+  } else {
+    user = await User.create(newUser);
+    res.json(user);
+  }
+});
 
 router.put("/user:userId", async function (req, res) {
     User.findByIdAndUpdate(req.params.userId, {
